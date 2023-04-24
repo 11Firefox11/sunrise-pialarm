a308a4da3c498bc2c19d02a878b0915f22bc9fc5
11Firefox11 <t.viktorvagyok@gmail.com>
2023-04-24 18:51:31 +0200

    Alarm runner config related base, env vars updated

---
+++ alarm_runner.py
@@ -0,0 +1,27 @@
+from led_controller import LedController
+from time import sleep as time_sleep
+from os import environ
+from json import load
+from jsonschema import validate as json_validate
+from jsonschema.exceptions import ValidationError
+from environment_variables import ALARM_JSONSCHEMA
+
+class AlarmRunner:
+    def __init__(self, config_path="alarm.json"):
+        self.config_path = config_path
+        self.parsed_config = load(open(self.config_path, "r", encoding="UTF-8"))
+        try:
+            json_validate(self.parsed_config, ALARM_JSONSCHEMA)
+        except ValidationError as err:
+            print(f"Alarm config was invalid (path: {self.config_path}). Message: {err.message}.")
+            exit(1)
+        self.load_config()
+
+    def load_config(self):
+        self.wait_time = self.parsed_config["wait_time"]
+        self.when_yellow = self.parsed_config["when_yellow"]
+        self.repeat_last = self.parsed_config.get("repeat_last")
+        self.steps = self.parsed_config["steps"]
+
+
+AlarmRunner()
--- environment_variables.py
+++ environment_variables.py
@@ -1,3 +1,55 @@
 DEV_MODE = True
 CODES_JSON_PATH = "codes.json"
 RGB_PINS = {"red": 17, "green": 22, "blue": 24}
+ALARM_MODIFY_REGEX_PATTERN = "^(\(|\)|\d+\.\d+|\d+|red|blue|green|[+\-*/%]|i|\s)*$"
+ALARM_JSONSCHEMA = {
+    "type": "object",
+    "properties": {
+        "wait_time": { "type": "number" },
+        "when_yellow": { "type": "number" },
+        "repeat_last": { "type": "number" },
+        "steps": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                "sleep": { "oneOf": [ { "type": "string" }, { "type": "number" } ] },
+                "range": {
+                    "type": "object",
+                    "properties": {
+                    "start": { "type": "number", "pattern": ALARM_MODIFY_REGEX_PATTERN },
+                    "stop": { "type": "number", "pattern": ALARM_MODIFY_REGEX_PATTERN },
+                    "step": { "type": "number", "pattern": ALARM_MODIFY_REGEX_PATTERN }
+                    },
+                    "required": [],
+                    "additionalProperties": False
+                },
+                "transition": {
+                    "type": "object",
+                    "properties": {
+                    "red": { "type": "number" },
+                    "green": { "type": "number" },
+                    "blue": { "type": "number" }
+                    },
+                    "required": [],
+                    "additionalProperties": False
+                },
+                "modify": {
+                    "type": "object",
+                    "properties": {
+                    "red": { "type": "string" },
+                    "green": { "type": "string" },
+                    "blue": { "type": "string" }
+                    },
+                    "required": [],
+                    "additionalProperties": False
+                }
+                },
+                "required": ["sleep"],
+                "additionalProperties": False
+            }
+        }
+    },
+    "required": ["wait_time", "when_yellow", "steps"],
+    "additionalProperties": False,
+}
