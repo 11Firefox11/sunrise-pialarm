28fe84fafdd8190115fd464b358b6b38aa1008a1
11Firefox11 <t.viktorvagyok@gmail.com>
2023-04-28 19:33:27 +0200

    led_controller init_dev_window added, rgb check round bug fix

--- led_controller.py
+++ led_controller.py
@@ -9,31 +9,17 @@ def rgb_value_checker(func):
     def wrapper(self, val):
         if val < 0: val = 0
         elif val > 255: val = 255
-        elif type(val) == float: val = floor(val)
-        return func(self, val)
+        return func(self, round(float(val)))
     return wrapper
 
 class LedController:
     def __init__(self, r: int = 0, g: int = 0, b: int = 0):
         self._r = self._g = self._b = 0
         self.pi = pi() if not DEV_MODE else None
-        if DEV_MODE:
-            def gui_thread():
-                try:
-                    window = sg.Window("Sunrise Pi Alarm Development", [[]], grab_anywhere=True, resizable=True, size=(100, 100))
-                    while True:
-                        event, values = window.Read(timeout=10)
-                        window.TKroot.configure(background="#{:02x}{:02x}{:02x}".format(self.red, self.green, self.blue))
-                        if event == sg.WIN_CLOSED:
-                            break
-                    window.Close()
-                except Exception as x:
-                    window.Close()
-            gui_thread = Thread(target=gui_thread, daemon=True)
-            gui_thread.start()
         self.red = r
         self.green = g
         self.blue = b
+        if DEV_MODE: self.init_dev_window()
 
     def reset(self): self.red = self.green = self.blue = 0
 
@@ -43,9 +29,9 @@ class LedController:
         if b == None: b = self.blue
         transition_colors = Color(rgb=(self.red/255, self.green/255, self.blue/255)).range_to(Color(rgb=(r/255, g/255, b/255)), steps)
         toReturn = []
-        for c in transition_colors:
-            if c not in toReturn: toReturn.append(c)
-        return [[255*c.red, 255*c.green, 255*c.blue] for c in toReturn]
+        for color in transition_colors:
+            if color not in toReturn: toReturn.append([c * 255 for c in color.rgb])
+        return toReturn
 
     @property
     def red(self): return self._r
@@ -73,3 +59,18 @@ class LedController:
     def blue(self, val):
         self._b = val
         if not DEV_MODE: self.pi.set_PWM_dutycycle(RGB_PINS["blue"], self._b)
+
+    def init_dev_window(self):
+        def gui_thread():
+            try:
+                window = sg.Window("Sunrise Pi Alarm Development", [[]], grab_anywhere=True, resizable=True, size=(100, 100))
+                while True:
+                    event, values = window.Read(timeout=10)
+                    window.TKroot.configure(background="#{:02x}{:02x}{:02x}".format(self.red, self.green, self.blue))
+                    if event == sg.WIN_CLOSED:
+                        break
+                window.Close()
+            except Exception as x:
+                window.Close()
+        gui_thread = Thread(target=gui_thread, daemon=True)
+        gui_thread.start()
